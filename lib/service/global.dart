3ef84e1c813e7ef0d36e333c1e855249778ff694
Firekepr <93929735+Firekepr@users.noreply.github.com>
2024-05-22 20:14:32 -0300

    New - global file

---
+++ lib/service/global.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/foundation.dart';
+
+class Global {
+  static bool isDev = kDebugMode;
+
+  static bool printSelectQuery = true;
+  static bool printInsertQuery = true;
+  static bool printUpdateQuery = true;
+  static bool printDeleteQuery = true;
+}
