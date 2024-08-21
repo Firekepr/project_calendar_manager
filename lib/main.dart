5551e336c10a65a898a157d2baf217bed9af2704
Firekepr <93929735+Firekepr@users.noreply.github.com>
2024-08-20 21:11:06 -0300

    New - install and configure provider lib

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:project_calendar_manager/pages/app/app.dart';
+import 'package:project_calendar_manager/providers/calendar_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+      MultiProvider(
+        providers: [ ChangeNotifierProvider<CalendarProvider>(create: (_) => CalendarProvider()) ],
+        child: const MyApp(),
+      ),
+  );
 }
