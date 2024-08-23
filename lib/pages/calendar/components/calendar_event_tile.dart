cdf74332ff2767f58eb7cbaa86c8cfe4296dadaf
Firekepr <93929735+Firekepr@users.noreply.github.com>
2024-08-22 21:33:32 -0300

    Update - favorite star layout

--- lib/pages/calendar/components/calendar_event_tile.dart
+++ lib/pages/calendar/components/calendar_event_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:icon_decoration/icon_decoration.dart';
 import 'package:project_calendar_manager/database/classes/events.dart';
 import 'package:project_calendar_manager/service/utils/utils.dart';
 import 'package:project_calendar_manager/styles/colors/app_colors.dart';
@@ -8,11 +9,13 @@ import 'package:project_calendar_manager/widget/simple.dart';
 class CalendarEventTile extends StatelessWidget {
   final EventsC item;
   final void Function() onFavorite;
+  final void Function() onTrash;
 
   const CalendarEventTile({
     super.key,
     required this.item,
     required this.onFavorite,
+    required this.onTrash,
   });
 
   @override
@@ -20,7 +23,8 @@ class CalendarEventTile extends StatelessWidget {
     return SimpleWidget.customContainer(
       borderWidth: 2,
       bdColor: AppColors.black,
-      margin: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 4.0),
+      margin: const EdgeInsets.symmetric(vertical: 2.0, horizontal: 4.0),
+      // padding: const EdgeInsets.only(bottom: 1.0),
       borderRadius: 6.0,
       child: ListTile(
         dense: true,
@@ -30,9 +34,17 @@ class CalendarEventTile extends StatelessWidget {
         leading: IconButton(
           onPressed: onFavorite,
           color: AppColors.yellow.color,
-          icon: Icon(
-            item.favorite ? Icons.star : Icons.star_border,
-            color: item.favorite ? AppColors.yellowDark.color : AppColors.black.color,
+          icon: DecoratedIcon(
+            icon: Icon(
+              item.favorite ? Icons.star : Icons.star_border,
+              color: item.favorite ? AppColors.yellowDark.color : AppColors.black.color,
+            ),
+            decoration: IconDecoration(
+              border: IconBorder(
+                color: Colors.black,
+                width: item.favorite ? 4.0 : 0,
+              ),
+            ),
           ),
         ),
       ),
